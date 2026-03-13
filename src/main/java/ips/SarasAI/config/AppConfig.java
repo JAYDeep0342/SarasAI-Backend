8062c331f05a6c6084a2c6b1082efe14be709e0e
Jaydeep0342 <jaydeep0342@gmail.com>
2026-03-13 14:28:47 +0530

    env solve

--- src/main/java/ips/SarasAI/config/AppConfig.java
+++ src/main/java/ips/SarasAI/config/AppConfig.java
@@ -6,10 +6,12 @@ package ips.SarasAI.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -27,6 +29,14 @@ public class AppConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    @Bean
+    public HttpHeaders httpHeaders(){
+        return  new HttpHeaders();
     }
 //    @Bean
 //    UserDetailsService userDetailsService() {
