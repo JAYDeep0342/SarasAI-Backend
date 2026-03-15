6ccbf457755dc9ab6e9f38c997a5d8cad1cecbea
Jaydeep0342 <jaydeep0342@gmail.com>
2026-03-15 21:59:38 +0530

    crosorigin

--- src/main/java/ips/SarasAI/config/AppConfig.java
+++ src/main/java/ips/SarasAI/config/AppConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -38,6 +43,7 @@ public class AppConfig {
     public HttpHeaders httpHeaders(){
         return  new HttpHeaders();
     }
+
 //    @Bean
 //    UserDetailsService userDetailsService() {
 //        UserDetails user1 = User.withUsername("admin")
